18be39ed06a5768b711ad19c7afc7f4818d60a86
neya0 <wltjd1484@gmail.com>
2022-09-01 21:56:12 +0900

    feat: 카카오 로그인 구현

--- src/main/java/com/four/withtopia/api/controller/MemberController.java
+++ src/main/java/com/four/withtopia/api/controller/MemberController.java
@@ -1,15 +1,14 @@
 package com.four.withtopia.api.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.four.withtopia.api.service.MemberService;
 import com.four.withtopia.dto.request.LoginRequestDto;
 import com.four.withtopia.dto.request.MemberRequestDto;
 import com.four.withtopia.dto.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,12 +20,12 @@ public class MemberController {
 
   private final MemberService memberService;
 
-  @RequestMapping(value = "/api/member/signup", method = RequestMethod.POST)
+  @RequestMapping(value = "/member/signup", method = RequestMethod.POST)
   public ResponseDto<?> signup(@RequestBody @Valid MemberRequestDto requestDto) {
     return memberService.createMember(requestDto);
   }
 
-  @RequestMapping(value = "/api/member/login", method = RequestMethod.POST)
+  @RequestMapping(value = "/member/login", method = RequestMethod.POST)
   public ResponseDto<?> login(@RequestBody @Valid LoginRequestDto requestDto,
       HttpServletResponse response
   ) {
@@ -38,8 +37,18 @@ public class MemberController {
 //    return memberService.reissue(request, response);
 //  }
 
-  @RequestMapping(value = "/api/auth/member/logout", method = RequestMethod.POST)
+  @RequestMapping(value = "/member/logout", method = RequestMethod.POST)
   public ResponseDto<?> logout(HttpServletRequest request) {
     return memberService.logout(request);
+  }
+
+  @RequestMapping(value = "/member/login/kakao", method = RequestMethod.GET)
+  public ResponseEntity<?> kakaoLogin(@RequestParam(name="code") String code, HttpServletResponse response) throws JsonProcessingException {
+    return memberService.kakaoLogin(code, response);
+  }
+
+  @RequestMapping(value = "/member/login/google", method = RequestMethod.GET)
+  public ResponseEntity<?> googleLogin(@RequestParam(name="code") String code, HttpServletResponse response) throws JsonProcessingException {
+    return memberService.googleLogin(code, response);
   }
 }
