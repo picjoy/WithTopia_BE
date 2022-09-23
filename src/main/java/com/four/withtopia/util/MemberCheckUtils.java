54e77d7596179e48904d578a8169ebd7dd1b425a
neya0 <wltjd1484@gmail.com>
2022-09-23 17:57:32 +0900

    <chore> MemberCheckUtils 탈퇴 회원 예외처리 수정

--- src/main/java/com/four/withtopia/util/MemberCheckUtils.java
+++ src/main/java/com/four/withtopia/util/MemberCheckUtils.java
@@ -1,8 +1,11 @@
 package com.four.withtopia.util;
 
+import com.four.withtopia.config.error.ErrorCode;
+import com.four.withtopia.config.expection.PrivateException;
 import com.four.withtopia.config.security.jwt.TokenProvider;
 import com.four.withtopia.db.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,18 +19,24 @@ public class MemberCheckUtils {
     public Member checkMember(HttpServletRequest request){
 
         if(request.getHeader("RefreshToken") == null){
-            throw new NullPointerException("로그인을 해주세요.");
+            throw new PrivateException(new ErrorCode(HttpStatus.NOT_FOUND,"404","로그인을 해주세요."));
         }
 
         if (!tokenProvider.validateToken(request.getHeader("RefreshToken"))) {
-            throw new IllegalArgumentException("Token이 유효하지 않습니다.");
+            throw new PrivateException(new ErrorCode(HttpStatus.BAD_REQUEST,"400","Token이 유효하지 않습니다."));
         }
 
         Member member = tokenProvider.getMemberFromAuthentication();
 
         if (null == member) {
-            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+            throw new PrivateException(new ErrorCode(HttpStatus.NOT_FOUND,"404","사용자를 찾을 수 없습니다."));
         }
+
+        // 멤버가 탈퇴한 회원인 경우
+        if (member.isDelete()){
+            throw new PrivateException(new ErrorCode(HttpStatus.NOT_FOUND,"404","탈퇴한지 3일이 경과하지 않았습니다."));
+        }
+
         return member;
     }
 }
