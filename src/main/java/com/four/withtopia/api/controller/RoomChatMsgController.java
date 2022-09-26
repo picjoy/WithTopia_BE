f3e4508c1e12000109bff72df57ac4db1c4daf43
neya0 <wltjd1484@gmail.com>
2022-09-26 12:21:42 +0900

    <feat> redis pub/sub 구현

--- src/main/java/com/four/withtopia/api/controller/RoomChatMsgController.java
+++ src/main/java/com/four/withtopia/api/controller/RoomChatMsgController.java
@@ -1,10 +1,14 @@
 package com.four.withtopia.api.controller;
 
 import com.four.withtopia.api.service.RoomChatMsgService;
+import com.four.withtopia.api.service.chat.RedisPublisher;
+import com.four.withtopia.api.service.chat.RedisSubscriber;
 import com.four.withtopia.db.domain.ChatMessage;
 import com.four.withtopia.dto.stomp.RoomChatMsgDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -15,14 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class RoomChatMsgController {
-
-    private final SimpMessageSendingOperations sendingOperations;
     private final RoomChatMsgService roomChatMsgService;
+    private final RedisMessageListenerContainer redisMessageListenerContainer;
+    private final RedisSubscriber redisSubscriber;
+    private final RedisPublisher redisPublisher;
 
     @MessageMapping("/chat/{roomId}")
     public void chat(@DestinationVariable String roomId, @RequestBody ChatMessage message){
         RoomChatMsgDto chatMessage = roomChatMsgService.createRoomChatMessage(roomId, message);
-        System.out.println("chatMessage = " + chatMessage.getSender() + "가 " + chatMessage.getMessage() + "라고 말함");
-        sendingOperations.convertAndSend("/topic/chat/"+ roomId, chatMessage);
+        ChannelTopic topic = new ChannelTopic(roomId);
+        redisMessageListenerContainer.addMessageListener(redisSubscriber, topic);
+        redisPublisher.publish(topic, chatMessage);
     }
 }
