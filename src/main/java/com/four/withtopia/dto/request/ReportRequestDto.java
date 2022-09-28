0db007a2bc7f0b704c11b54ae2de2d4d398a4ee2
Jo Wonyoung <ckstn0225@Gmail.com>
2022-09-28 22:11:35 +0900

    <Wonyoung> <fix> fix report

--- src/main/java/com/four/withtopia/dto/request/ReportRequestDto.java
+++ src/main/java/com/four/withtopia/dto/request/ReportRequestDto.java
@@ -7,8 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 @RequiredArgsConstructor
 public class ReportRequestDto {
-    private String toNickname;
-    private String byNickname;// 신고 할사람
+    private String toNickname;// 신고 할사람
     private String content; // 신고 내용
     private MultipartFile image;
 }
